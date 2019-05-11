809fc4bebc5d58a321b434e9a6b5ff9f717e5d24
calidion <calidion@gmail.com>
2019-05-11 12:29:34 +0800

    init update

--- src/main.js
+++ src/main.js
@@ -1,8 +1,37 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import {
+  VueI18n,
+  HTTPLoader
+} from "node-i18n-vue";
+
 Vue.config.productionTip = false
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+const options = {
+  current: 'en',
+  url: "http://localhost:8080/locales/",
+  loader: HTTPLoader
+};
+
+Vue.use(VueI18n, {
+  options,
+  storage: window.localStorage
+});
+
+let i18n = Vue.getI18n();
+
+i18n.init().then(() => {
+  new Vue({
+    render: h => h(App),
+  }).$mount('#app')
+});
+
+
+const locales = ['zh-CN', 'en'];
+let count = (Math.random() * 100).toFixed(0);
+setInterval(() => {
+  Vue.setLocale(locales[count++ % 2]).then(() => {
+    window.location.reload();
+  });
+}, 2000)
